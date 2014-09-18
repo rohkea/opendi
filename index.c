ababbebcc42fcd218dda165fe07ec62f925bf32e
rohkea <rohkea@users.noreply.github.com>
2014-09-18 23:37:25 +0300

    Saving properties to memory

--- index.c
+++ index.c
@@ -101,8 +101,69 @@ void idxParseIndex(IndexReaderData *ird) {
 	DEBUG_PRINT1("Got line \"%s\"\n", line);
 }
 
-bool idxParseLine(IndexReaderData *ird) {
+int idxFindProp(IndexReaderData *ird, char *propName) {
 	int i;
+	
+	for (i = 0; i < ird->numProps; i++) {
+		if (strcmp(ird->props[i].name, propName) == 0) {
+			return i;
+		}
+	}
+	
+	return -1;
+}
+
+/* name and value are malloc’d strings that should be freed */
+void idxCreateProp(IndexReaderProperty *prop, char *name, char *value) {
+	DEBUG_PRINT2("CREATING A PROPERTY %s = '%s'\n", name, value);
+	prop->name = name;
+	prop->allocdValues = 1;
+	prop->numValues = 1;
+	prop->values = malloc(sizeof(char *));
+	prop->values[0] = value;
+}
+
+void idxAppendPropValue(IndexReaderProperty *prop, char *value) {
+	/* TODO: start everything anew if property read from a new file */
+	DEBUG_PRINT2("APPENDING %s += '%s'\n", prop->name, value);
+	if (prop->numValues >= prop->allocdValues) {
+		prop->allocdValues *= 2;
+		realloc(prop->values, sizeof(char *) *
+					prop->allocdValues);
+	}
+	prop->values[prop->numValues] = value;
+	prop->numValues++;
+}
+
+/* name and value are malloc’d strings that should be freed */
+void idxAppendProp(IndexReaderData *ird, char *name, char *value) {
+	int propIndex;
+	IndexReaderProperty *prop;
+	
+	propIndex = idxFindProp(ird, name);
+	if (propIndex == -1) {
+		/* TODO: allocate the new property */
+		if (ird->numProps >= ird->allocdProps) {
+			ird->allocdProps *= 2;
+			realloc(ird->props, sizeof(IndexReaderProperty) *
+						ird->allocdProps);
+		}
+		
+		idxCreateProp(&ird->props[ird->numProps], name, value);
+		ird->lastProp = ird->numProps;
+		
+		ird->numProps++;
+	}
+	else {
+		/* TODO: check if property was read from a different file */
+		free(name);
+		idxAppendPropValue(&ird->props[propIndex], value);
+		ird->lastProp = propIndex;
+	}
+}
+
+bool idxParseLine(IndexReaderData *ird) {
+	int i, propIndex;
 	char *propName, *propValue;
 	
 	DEBUG_PRINT1("Parsing line, current position is %d\n",
@@ -113,6 +174,15 @@ bool idxParseLine(IndexReaderData *ird) {
 		DEBUG_PRINT0("Continuing previous property...");
 		propValue = idxReadPropValue(ird);
 		DEBUG_PRINT1("Value: %s\n", propValue);
+		
+		if (ird->lastProp > -1) {
+			DEBUG_PRINT1("Continuing prop No. %d\n", ird->lastProp);
+			idxAppendPropValue(&ird->props[ird->lastProp], propValue);
+		}
+		else {
+			/* No last prop? Then just drop the value */
+			free(propValue);
+		}
 	}
 	else {
 		DEBUG_PRINT0("Staring a new property...\n");
@@ -122,6 +192,19 @@ bool idxParseLine(IndexReaderData *ird) {
 		propValue = idxReadPropValue(ird);
 		DEBUG_PRINT1("Property value: %s\n", propValue);
 		/* TODO: check if it’s not in use already and add/append */
+		
+		idxAppendProp(ird, propName, propValue);
+	}
+}
+
+void idxCleanIRD(IndexReaderData *ird) {
+	if (ird->buffer) {
+		free(ird->buffer);
+	}
+	
+	if (ird->props) {
+		/* todo: free each property’s values */
+		free(ird->props);
 	}
 }
 
@@ -144,8 +227,13 @@ DictionaryData *idxLoadDict(TCHAR *filename) {
 	ird.buffer = malloc(fileSize);
 	if(!ReadFile(hFile, ird.buffer, fileSize, &read, NULL)) {
 		CloseHandle(hFile);
+		free(ird.buffer);
 		return NULL;
 	}
+	ird.allocdProps = 8;
+	ird.numProps = 0;
+	ird.lastProp = -1;
+	ird.props = malloc(sizeof(IndexReaderProperty) * ird.allocdProps);
 	
 	CloseHandle(hFile);
 	
@@ -166,5 +254,6 @@ DictionaryData *idxLoadDict(TCHAR *filename) {
 	/* TODO: finish this */
 	
 	free(ird.buffer);
+	idxCleanIRD(&ird);
 	return NULL;
 }
